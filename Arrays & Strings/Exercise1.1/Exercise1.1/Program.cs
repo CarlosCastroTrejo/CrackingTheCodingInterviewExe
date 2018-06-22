2f2648bbc755999cbfb67360deb1c1574ac76feb
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2018-06-21 22:05:16 -0500

    Array & Strings module was added, exercise 1.1 is done

---
+++ Arrays & Strings/Exercise1.1/Exercise1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercise1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine(CheckUnique("abcdaefghijklm"));
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        public static bool CheckUnique(string word)
+        {
+            for (int x = 0; x < word.Length;x++)
+            {
+                for (int y = 0; y < word.Length; y++)
+                {
+                    if (x!=y)
+                    {
+                        if(word[x]==word[y])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        } 
+    }
+}
